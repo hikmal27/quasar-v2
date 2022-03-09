37d93d54e22945b8687daa2b105a003e6b9ef510
Hikmal Maulana Ramadhan <hikmal.ramadhan@smartfinance.co.id>
2022-03-09 22:46:45 +0700

    First commit

---
+++ src/pages/Testing.vue
@@ -0,0 +1,5 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="tw-font-quicksand">
+    <p class="tw-text-2xl tw-font-normal">Hikmal Maulana R</p>
+  </q-layout>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      quicksand: ['Quicksand', 'sans-serif'],
+    }
+  },
+  prefix: 'tw-',
+  plugins: [],
+}
